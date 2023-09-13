61f87955860622318bd94f8e1fd7d4a5b666ea77
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-13 09:51:52 -0700

    setting up webpack, still working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import svgLogo from './images/logo.svg';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  //add logo
+  const logo = new Image();
+  logo.src = svgLogo;
+
+  element.appendChild(logo);
 
   return element;
 }
