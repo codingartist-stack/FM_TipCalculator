84f9b99446feac61dfab470ee388c02f70d8d730
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-13 09:16:42 -0700

    still setting up webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import svgLogo from './images/logo.svg';
 
 function testElement() {
   const element = document.createElement('div');
 
   element.innerText = 'testing CSS';
   element.classList.add('hello');
+
+  const logo = new Image();
+  logo.src = svgLogo;
+
+  element.appendChild(logo);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -11,6 +13,14 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
